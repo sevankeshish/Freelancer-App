700e4fc08f3cb7e8aa4093fa3d2edceed1d5d4c5
Hexact task for Junior React JS <sevan.keshish0@gmail.com>
2024-09-29 14:23:24 -0700

    adding tailwind collor

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold">Start new project</h1>;
+  return (
+    <h1 className="container xl:max-w-screen-xl bg-opacity-20 bg-primary-900 text-3xl font-bold">
+      Start new project
+    </h1>
+  );
 }
 
 export default App;
