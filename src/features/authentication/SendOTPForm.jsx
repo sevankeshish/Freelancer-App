f517295eb2c70e0de9f1f13ac4c59b303291d4a5
Hexact task for Junior React JS <sevan.keshish0@gmail.com>
2024-10-01 22:46:01 -0700

    customize tailwind classes

--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -14,16 +14,11 @@ function SendOTPForm() {
             value={phoneNumber}
             onChange={(e) => setPhoneNumber(e.target.value)}
             id="phonenumber"
-            className="w-full py-3 px-4 rounded-xl text-secondary-900 border border-gray-300 
-            hover:border-primary-500 focus:border-primary-500 focus:bg-white transition-all
-            duration-300 ease-out focus:shadow-lg focus:shadow-primary-200"
+            className="textField__input"
             type="text"
           />
         </div>
-        <button
-          className="px-4 py-2 font-bold bg-primary-900 text-white w-full 
-          rounded-xl trasition-all durration-300 hover:bg-primary-800 shadow-lg shadow-primary-300"
-        >
+        <button className="btn btn--primary w-full">
           send the authentication code
         </button>
       </form>
