44bd685872943d793cf94f52707c6ec5e1a3d75b
Hexact task for Junior React JS <sevan.keshish0@gmail.com>
2024-10-03 18:06:19 -0700

    creating text-field dynamic

--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import { useState } from "react";
+import TextField from "../../ui/TextField";
 
 function SendOTPForm() {
   const [phoneNumber, setPhoneNumber] = useState("");
   return (
     <div>
       <form className="space-y-8">
-        <div>
-          <label className="mb-1" htmlFor="phonenumber">
-            phone number
-          </label>
-          <input
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            id="phonenumber"
-            className="textField__input"
-            type="text"
-          />
-        </div>
+        <TextField
+          label="phone number"
+          name="phoneNumber"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
         <button className="btn btn--primary w-full">
           send the authentication code
         </button>
