16c57b3c92f0340aa2871d18778899c3f6a79967
Hexact task for Junior React JS <sevan.keshish0@gmail.com>
2024-10-13 15:55:30 -0700

    Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="h-screen bg-secondary-0">
+      <div className="container xl:max-w-screen-xl">
+        <hi className="p-4 text-xl text-secondary-700">Home</hi>
+      </div>
+    </div>
+  );
+}
+
+export default Home();
