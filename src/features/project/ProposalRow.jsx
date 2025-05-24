e2a626779c0485a3adeb3751b62cd88473a8dd87
sevan keshish <sevan.keshish0@gmail.com>
2025-05-23 22:05:24 -0700

    bug fix

--- src/features/project/ProposalRow.jsx
+++ src/features/project/ProposalRow.jsx
@@ -2,15 +2,18 @@ import Table from "../../ui/Table";
 import truncateText from "../../utils/truncateText";
 
 function ProposalRow({ propsal, index }) {
+  const { status } = propsal;
+  const color = status === 2 ? "primary" : "danger";
+
   return (
     <Table.Row>
       <td>{index + 1}</td>
       <td>{propsal.user.name}</td>
       <td>{truncateText(propsal.description, 50)}</td>
-      <td>{propsal.duration}</td>
+      <td>{propsal.duration} day</td>
       <td>{propsal.price}</td>
       <td>
-        <span>{propsal.status}</span>
+        <span className={`badge bg--${color}`}>{propsal.status}</span>
       </td>
       <td>++</td>
     </Table.Row>
