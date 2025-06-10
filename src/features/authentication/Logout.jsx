c0c046fd76e19ccc2f066977ef50d615657b7fff
sevan keshish <sevan.keshish0@gmail.com>
2025-06-09 20:00:36 -0700

    ui for log out page

---
+++ src/features/authentication/Logout.jsx
@@ -0,0 +1,11 @@
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
+
+function Logout() {
+  return (
+    <button>
+      <HiArrowRightOnRectangle className="w-5 h-5 text-secondary-500 hover:text-error" />
+    </button>
+  );
+}
+
+export default Logout;
--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -1,5 +1,24 @@
+import { HiOutlineUser } from "react-icons/hi2";
+import { Link } from "react-router-dom";
+import Logout from "../features/authentication/Logout";
+import DarkModeToggle from "./DarkModeToggle";
+
 function HeaderMenu() {
-  return <div>HeaderMenu</div>;
+  return (
+    <ul className="flex gap-x-4 items-center">
+      <li className="flex">
+        <Link to="dashboard">
+          <HiOutlineUser className="w-5 h-5 text-primary-900" />
+        </Link>
+      </li>
+      <li className="flex">
+        <DarkModeToggle />
+      </li>
+      <li className="flex">
+        <Logout />
+      </li>
+    </ul>
+  );
 }
 
 export default HeaderMenu;
